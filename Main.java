39f1697e759d1bf5b565a40b9cf82a790048d0fe
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-10-01 16:58:02 -0400

    developed main; defined variables; assigned variables to scnr inputs; created new instance of a vendingMachine object; called vending machine object methods using variables as parameters;

--- Main.java
+++ Main.java
@@ -4,7 +4,19 @@ public class Main {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       
-      // TODO: Type your code here.
-
+      int numToPurchase;
+      int numToPurchase2;
+      int numToRestock;
+      
+      numToPurchase = scnr.nextInt();
+      numToRestock = scnr.nextInt();
+      numToPurchase2 = scnr.nextInt();
+      
+      VendingMachine vendingMachine = new VendingMachine();
+      vendingMachine.purchase(numToPurchase);
+      vendingMachine.restock(numToRestock);
+      vendingMachine.purchase(numToPurchase2);
+      
+      vendingMachine.report();
    }
 }
